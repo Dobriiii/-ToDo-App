49d042be5d1d29b9fb57e4e0a75b353dcd12fdd6
Dobriiii <dilev8g@gmail.com>
2022-05-31 18:16:03 +0300

    The LoginController and RegisterController are done!

---
+++ ToDo Application/Controller/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_Application.Controller
+{
+    public class LoginController
+    {
+        public string ShowMessage(string username, string password)
+        {
+            using (ToDoAppDbEntities ex = new ToDoAppDbEntities())
+            {
+                var existingUser = ex.UserTables.Where(u => u.Username == username).FirstOrDefault();
+
+                if (existingUser != null)
+                {
+                    if (existingUser.Password == password)
+                    {
+                      
+                        return null;
+
+                    }
+
+                    else
+                    {
+
+                        return "Wrong Password!";
+
+                    }
+                }
+                else
+                {
+                    return "No such user!";
+                }
+            }
+        }
+    }
+}
